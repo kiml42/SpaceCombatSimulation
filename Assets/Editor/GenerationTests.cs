66ed95de5edf571282180228a64dcb89e53c1870
Richard Clacksman <kiml42a@gmail.com>
2017-09-20 10:26:51 +0100

    Actually made a test- and it passes.

---
+++ Assets/Editor/GenerationTests.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using Assets.src.Evolution;
+
+public class GenerationTests
+{
+
+    [Test]
+    public void RecordMatch_SavesCombatants()
+    {
+        var gen = new Generation();
+        var a = "a";
+        var b = "b";
+        gen.AddGenome(a);
+        gen.AddGenome(b);
+        gen.AddGenome("blancmonge");
+
+        gen.RecordMatch(a, b, a, 13, 7, 5);
+
+        var genString = gen.ToString();
+
+        Assert.True(genString.Contains("a;13;1;0;0;b"), "a's line is wrong");
+        Assert.True(genString.Contains("b;7;0;0;1;a"), "b's line is wrong");
+    }
+
+    //// A UnityTest behaves like a coroutine in PlayMode
+    //// and allows you to yield null to skip a frame in EditMode
+    //[UnityTest]
+    //public IEnumerator TestTestWithEnumeratorPasses()
+    //{
+    //    // Use the Assert class to test conditions.
+    //    // yield to skip a frame
+    //    yield return null;
+    //}
+}
