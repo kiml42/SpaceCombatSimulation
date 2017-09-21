8a66735b3fe0800c7fcf27a2dca806eb6cb3d3c0
Endarren <nhirwin0@yahoo.com>
2017-09-21 12:17:09 -0600

    Add files via upload
    This is a script for projectiles to put them as children of a Projectile game object when they are created in the scene.

---
+++ Assets/src/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile :MonoBehaviour
+{
+	public static GameObject projectileParent;
+	// Use this for initialization
+	void Start ()
+	{
+		if (projectileParent == null)
+		{
+			projectileParent = new GameObject("Projectiles");
+		}
+		transform.SetParent(projectileParent.transform);
+	}
+	
+}
