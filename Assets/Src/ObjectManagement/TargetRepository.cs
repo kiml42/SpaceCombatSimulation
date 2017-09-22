8517eddc40c6372ed883ad8f3cd4ecd8082f2941
Richard Clacksman <kiml42a@gmail.com>
2017-09-22 15:49:45 +0100

    Started work on target repository.

---
+++ Assets/Src/ObjectManagement/TargetRepository.cs
@@ -0,0 +1,58 @@
+﻿using Assets.Src.Targeting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Src.ObjectManagement
+{
+    public static class TargetRepository
+    {
+        private static Dictionary<string, List<PotentialTarget>> _targets;
+
+        public static void RegisterTarget(PotentialTarget target)
+        {
+            if(target != null && target.TargetTransform!= null && target.TargetTransform.IsValid())
+            {
+                var tag = target.TargetTransform.tag;
+                List<PotentialTarget> list = null;
+                if (!_targets.ContainsKey(tag) | _targets[tag] == null)
+                {
+                    list = new List<PotentialTarget>();
+                    _targets[tag] = list;
+                } else
+                {
+                    list = _targets[tag];
+                }
+                list.Add(target);
+
+                _targets[tag] = CleanList(list);
+            }
+        }
+
+        public static List<PotentialTarget> ListTargetsForTags(IEnumerable<string> tags)
+        {
+            var list = new List<PotentialTarget>();
+            foreach (var tag in tags)
+            {
+                if (_targets.ContainsKey(tag))
+                {
+                    list.AddRange(CleanList(_targets[tag]));
+                }
+            }
+            return list;
+        }
+
+        private static List<PotentialTarget> CleanList(List<PotentialTarget> list)
+        {
+            if(list == null)
+            {
+                return new List<PotentialTarget>();
+            }
+            return  list
+                .Where(target => target != null && target.TargetTransform != null && target.TargetTransform.IsValid())
+                .Distinct()
+                .ToList();
+        }
+    }
+}
---
+++ Assets/Src/Targeting/RepositoryTargetDetector.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Src.Interfaces;
+using Assets.Src.ObjectManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Src.Targeting
+{
+    public class RepositoryTargetDetector : ITargetDetector
+    {
+        public IEnumerable<string> EnemyTags = new List<string> { "Enemy" };
+        public float ProjectileSpeed = 0;
+
+        public RepositoryTargetDetector()
+        {
+
+        }
+
+        public IEnumerable<PotentialTarget> DetectTargets()
+        {
+            return TargetRepository.ListTargetsForTags(EnemyTags);
+        }
+    }
+}
