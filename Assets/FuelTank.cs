97540df4adb97d98983f31cb32909e1606c677d0
Richard Clacksman <kiml42a@gmail.com>
2017-09-22 14:55:52 +0100

    Added option for fuel to have mass

--- Assets/FuelTank.cs
+++ Assets/FuelTank.cs
@@ -6,6 +6,27 @@ using UnityEngine;
 public class FuelTank : MonoBehaviour {
     public float Fuel;
 
+    public bool UseFuelMass = false;
+    /// <summary>
+    /// Mass will be added to the Rigidbody(if present) = to FuelDensity * Fuel.
+    /// 1gram per unit by default.
+    /// </summary>
+    [Tooltip("In kg per unit")]
+    public float FuelDensity = 0.001f;
+
+    private float _originalMass;
+    private Rigidbody _rigidbody;
+
+    public void Start()
+    {
+        if (UseFuelMass)
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _originalMass = _rigidbody.mass;
+            SetMassIncludingFuel();
+        }
+    }
+
     public float DrainFuel(float requestedFuel)
     {
         if(Fuel <= 0)
@@ -14,6 +35,15 @@ public class FuelTank : MonoBehaviour {
         }
         var fuelToReturn = Math.Min(requestedFuel, Fuel);
         Fuel -= fuelToReturn;
+        if (UseFuelMass)
+        {
+            SetMassIncludingFuel();
+        }
         return fuelToReturn;
+    }
+
+    private void SetMassIncludingFuel()
+    {
+        _rigidbody.mass = _originalMass + Fuel * FuelDensity;
     }
 }
