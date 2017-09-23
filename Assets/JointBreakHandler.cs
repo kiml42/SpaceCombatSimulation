2de4cf979682ef8e786f6a33c132a3fa0367331c
Richard Clacksman <kiml42a@gmail.com>
2017-09-23 14:58:45 +0100

    Created jointbreak handler

---
+++ Assets/JointBreakHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointBreakHandler : MonoBehaviour {
+    public bool DeactivateParent = false;
+    public bool DeactivateOnBreak = true;
+
+    void OnJointBreak(float breakForce)
+    {
+        Debug.Log("A joint has just been broken!, force: " + breakForce);
+        if (DeactivateOnBreak)
+        {
+            Deactivate();
+        }
+    }
+
+    public void Deactivate()
+    {
+        transform.SendMessage("Deactivate", SendMessageOptions.DontRequireReceiver);
+        if (DeactivateParent && transform.parent != null)
+        {
+            transform.parent.SendMessage("Deactivate", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
