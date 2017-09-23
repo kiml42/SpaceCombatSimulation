f2a628ccdf9b9672d62532765aa10e7fea8da90d
Richard Clacksman <kiml42a@gmail.com>
2017-09-23 15:11:07 +0100

    Made break handler do more.

--- Assets/JointBreakHandler.cs
+++ Assets/JointBreakHandler.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class JointBreakHandler : MonoBehaviour {
     public bool DeactivateParent = false;
     public bool DeactivateOnBreak = true;
+    public bool DisconectFromParent = true;
+    public Rigidbody BreakExplosion;
+    private bool _active = true;
 
     void OnJointBreak(float breakForce)
     {
-        Debug.Log("A joint has just been broken!, force: " + breakForce);
+        //Debug.Log(transform.name + "'s joint has just been broken!, force: " + breakForce);
+        if (DisconectFromParent)
+        {
+            transform.parent = null;
+        }
+        if (BreakExplosion != null)
+        {
+            var boom = Instantiate(BreakExplosion);
+            var rb = GetComponent("Rigidbody") as Rigidbody;
+            if (rb != null)
+                boom.velocity = rb.velocity;
+        }
         if (DeactivateOnBreak)
         {
             Deactivate();
@@ -17,7 +31,11 @@ public class JointBreakHandler : MonoBehaviour {
 
     public void Deactivate()
     {
-        transform.SendMessage("Deactivate", SendMessageOptions.DontRequireReceiver);
+        if (_active)
+        {
+            _active = false;
+            transform.SendMessage("Deactivate", SendMessageOptions.DontRequireReceiver);
+        }
         if (DeactivateParent && transform.parent != null)
         {
             transform.parent.SendMessage("Deactivate", SendMessageOptions.DontRequireReceiver);
