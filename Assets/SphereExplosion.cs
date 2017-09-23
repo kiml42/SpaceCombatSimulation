88a838cc119aa5aac1d2defc13b80845efe14e14
Richard Clacksman <kiml42a@gmail.com>
2017-09-23 10:03:54 +0100

    Started work on explosion spheres

---
+++ Assets/SphereExplosion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereExplosion : MonoBehaviour {
+    public Transform ExplosionSphere;
+    public float ExpandRate;
+    private Vector3 _expansion;
+
+    public float Lifetime = 4;
+    
+    void Start () {
+        ExplosionSphere.localScale = Vector3.zero;
+        _expansion = Vector3.one * ExpandRate;
+	}
+	
+	void FixedUpdate () {
+        ExplosionSphere.localScale += _expansion;
+        if (Lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+        Lifetime--;
+    }
+}
