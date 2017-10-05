86159d09718e212e91203fa852d5de8760a5934b
Richard Clacksman <kiml42a@gmail.com>
2017-10-05 19:22:53 +0100

    Added shipType

---
+++ Assets/Src/Targeting/ShipType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Src.Targeting
+{
+    public enum ShipType
+    {
+        TinyMunitions = 0,
+        SmallMunitions = 1,
+        LargeMunitions = 2,
+        Fighter = 3,
+        Corvette = 4,
+        Turret = 5,
+        Frigate = 6,
+        Capital = 7,
+        SuperCapital = 8
+    }
+}
---
+++ Assets/TypeKnower.cs
@@ -0,0 +1,8 @@
+﻿using Assets.Src.Targeting;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeKnower : MonoBehaviour {
+    public ShipType Type = ShipType.Frigate;
+}
