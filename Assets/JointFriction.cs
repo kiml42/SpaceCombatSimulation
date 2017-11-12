caab3547b592eddc88274c42082e808182a6c8c3
Richard Clacksman <kiml42a@gmail.com>
2017-11-12 12:44:29 +0000

    Started creating friction controler

---
+++ Assets/JointFriction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointFriction : MonoBehaviour {
+
+    [Tooltip("mulitiplier for the angular velocity for the torque to apply.")]
+    public float Friction = 1;
+
+    private HingeJoint _hinge;
+    private Rigidbody _thisBody;
+    private Rigidbody _connectedBody;
+    private Vector3 _axis;  //local space
+    private Vector3 _anchor;    //local space
+
+	// Use this for initialization
+	void Start () {
+        _hinge = GetComponent<HingeJoint>();
+        _connectedBody = _hinge.connectedBody;
+        _axis = _hinge.axis;
+        _anchor = _hinge.anchor;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var angularV = _hinge.velocity;
+        Debug.Log("angularV " + angularV);
+        var torque = Friction * angularV * _axis;
+
+        _thisBody.AddRelativeTorque(-torque);
+        //_connectedBody.AddRelativeTorque(-torque);
+
+    }
+}
