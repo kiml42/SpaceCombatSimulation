ffce96bb74555aaf270d9682c7b039638392f01a
Richard Clacksman <kiml42a@gmail.com>
2017-11-18 15:40:15 +0000

    Created controler for hit effect

---
+++ Assets/LampAndParticlesEffectController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampAndParticlesEffectController : MonoBehaviour {
+    public ParticleSystem Plume;
+    public Light Lamp;
+
+    private bool _active = true;
+
+    public float _fullPlumeRate;
+    private float _intensityScaler;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Plume != null)
+        {
+            _fullPlumeRate = Plume.emission.rateOverTime.constant;
+        }
+        if(Lamp != null)
+        {
+            _intensityScaler = Lamp.intensity;
+        }
+    }
+    
+    public void TurnOn(float proportion = 1)
+    {
+        _active = true;
+        SetPlumeState(1);
+    }
+
+    public void TurnOff()
+    {
+        _active = false;
+        SetPlumeState(0);
+    }
+
+    private void SetPlumeState(float proportion)
+    {
+        if(Plume != null)
+        {
+            if (proportion > 0)
+            {
+                //Debug.Log("turning plume on");
+                Plume.Play();
+
+                //reduce rate for throttle.
+                var emission = Plume.emission;
+                var rate = emission.rateOverTime;
+                rate.constant = _fullPlumeRate * proportion;
+                emission.rateOverTime = rate;
+            }
+            else
+            {
+                //Debug.Log("turning plume off");
+                Plume.Stop();
+            }
+        }
+        if(Lamp != null)
+        {
+            Lamp.intensity -= _intensityScaler * proportion;
+        }
+    }
+
+}
