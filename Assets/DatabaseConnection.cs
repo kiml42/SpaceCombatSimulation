5e60238af7ce75d164f5df8411454d18b2f6d7d8
Richard Clacksman <kiml42a@gmail.com>
2017-11-25 11:04:00 +0000

    Created database and test classes to interact with it.

---
+++ Assets/DatabaseConnection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+
+public class DatabaseConnection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string conn = "URI=file:" + Application.dataPath + "/SpaceCombatSimulationDB.s3db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT F1 " + "FROM Test";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            int f1 = reader.GetInt32(0);
+
+            Debug.Log("f1= " + f1);
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Src/Database/EvolutionTargetShootingDatabaseHandler.cs
@@ -0,0 +1,50 @@
+﻿using Mono.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Src.Database
+{
+    public class EvolutionTargetShootingDatabaseHandler
+    {
+        private EvolutionTargetShootingControler _toConfigure;
+
+        public EvolutionTargetShootingDatabaseHandler(EvolutionTargetShootingControler toConfigure)
+        {
+            _toConfigure = toConfigure;
+        }
+
+
+        public void ReadDroneConfig()
+        {
+            string conn = "URI=file:" + Application.dataPath + "/SpaceCombatSimulationDB.s3db"; //Path to database.
+            IDbConnection dbconn;
+            dbconn = (IDbConnection)new SqliteConnection(conn);
+            dbconn.Open(); //Open connection to the database.
+            IDbCommand dbcmd = dbconn.CreateCommand();
+            string sqlQuery = "SELECT F1 " + "FROM DroneEvolutionConfig";
+            dbcmd.CommandText = sqlQuery;
+            IDataReader reader = dbcmd.ExecuteReader();
+            while (reader.Read())
+            {
+                int f1 = reader.GetInt32(0);
+
+                Debug.Log("f1= " + f1);
+            }
+            reader.Close();
+            reader = null;
+            dbcmd.Dispose();
+            dbcmd = null;
+            dbconn.Close();
+            dbconn = null;
+        }
+
+        private void Disconect()
+        {
+
+        }
+    }
+}
