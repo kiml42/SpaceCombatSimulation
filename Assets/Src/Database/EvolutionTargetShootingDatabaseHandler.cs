bff416183f392a743acf9ee89959b53c8f89b655
Richard Clacksman <kiml42a@gmail.com>
2017-12-15 12:39:08 +0000

    Refactored out shared code from handlers

--- Assets/Src/Database/EvolutionTargetShootingDatabaseHandler.cs
+++ Assets/Src/Database/EvolutionTargetShootingDatabaseHandler.cs
@@ -10,35 +10,21 @@ using Assets.Src.Evolution;
 
 namespace Assets.Src.Database
 {
-    public class EvolutionTargetShootingDatabaseHandler
+    public class EvolutionTargetShootingDatabaseHandler : GeneralDatabaseHandler
     {
-        private const string DEFAULT_COMMAND_PATH = "/Database/CreateBlankDatabase.sql";
-        private const string DEFAULT_DB_PATH = "/tmp/SpaceCombatSimulationDB.s3db";
-        private string _connectionString
+        private const string CONFIG_TABLE = "DroneEvolutionConfig";
+        private const string INDIVIDUAL_TABLE = "DroneShootingIndividual";
+
+        public EvolutionTargetShootingDatabaseHandler(string databasePath, string dbCreationCommandPath):base(databasePath, dbCreationCommandPath)
         {
-            get
-            {
-                var connection = "URI=file:" + Application.dataPath + _databasePath;
-                //Debug.Log("connection string: " + connection);
-                return connection;
-            }
         }
-        private  string _databasePath; //Path to database.
 
-        public EvolutionTargetShootingDatabaseHandler(string databasePath = DEFAULT_DB_PATH, string dbCreationCommandPath = DEFAULT_COMMAND_PATH)
+        public EvolutionTargetShootingDatabaseHandler(string databasePath) : base(databasePath)
         {
-            //Debug.Log("EvolutionTargetShootingDatabaseHandler constructor");
+        }
 
-            _databasePath = databasePath;
-
-            if (!string.IsNullOrEmpty(dbCreationCommandPath))
-            {
-                var initialiser = new DatabaseInitialiser
-                {
-                    DatabasePath = _databasePath
-                };
-                initialiser.EnsureDatabaseExists(dbCreationCommandPath);
-            }
+        public EvolutionTargetShootingDatabaseHandler() : base()
+        {
         }
 
         public EvolutionTargetShootingConfig ReadConfig(int id)
@@ -52,16 +38,8 @@ namespace Assets.Src.Database
                 IDataReader reader = null;
                 try
                 {
-                    sql_con.Open(); //Open connection to the database.
-                    dbcmd = sql_con.CreateCommand();
-                    string sqlQuery = "SELECT *" +
-                        " FROM DroneEvolutionConfig" +
-                        " LEFT JOIN MatchConfig on MatchConfig.id = DroneEvolutionConfig.matchConfigId" +
-                        " LEFT JOIN MutationConfig on MutationConfig.id = DroneEvolutionConfig.mutationConfigId" +
-                        " WHERE DroneEvolutionConfig.id = " + id + ";";
-                    //Debug.Log(sqlQuery);
-                    dbcmd.CommandText = sqlQuery;
-                    reader = dbcmd.ExecuteReader();
+                    reader = OpenReaderWithCommant(sql_con, CreateReadConfigQuery(CONFIG_TABLE, id), out dbcmd);
+
                     reader.Read();
 
                     //Debug.Log("DroneEvolutionConfig.id ordinal: " + reader.GetOrdinal("id"));
@@ -82,19 +60,9 @@ namespace Assets.Src.Database
                     config.DeathPenalty = reader.GetFloat(reader.GetOrdinal("deathPenalty"));  //11
                     config.DronesString = reader.GetString(reader.GetOrdinal("droneList"));   //14
 
-                    //Debug.Log("matchConfigId ordinal: " + reader.GetOrdinal("MatchConfig.Id"));  //-1
-                    config.MatchConfig.Id = reader.GetInt32(12);
-                    config.MatchConfig.MatchTimeout = reader.GetFloat(reader.GetOrdinal("matchTimeout")); //16
-                    config.MatchConfig.WinnerPollPeriod = reader.GetFloat(reader.GetOrdinal("winnerPollPeriod")); //17
 
-                    config.MutationConfig.Id = reader.GetInt32(13);
-                    config.MutationConfig.Mutations = reader.GetInt32(reader.GetOrdinal("mutations"));    //19
-                    config.MutationConfig.AllowedCharacters = reader.GetString(reader.GetOrdinal("allowedCharacters"));   //20
-                    config.MutationConfig.MaxMutationLength = reader.GetInt32(reader.GetOrdinal("maxMutationLength"));   //21
-                    config.MutationConfig.GenomeLength = reader.GetInt32(reader.GetOrdinal("genomeLength")); //22
-                    config.MutationConfig.GenerationSize = reader.GetInt32(reader.GetOrdinal("generationSize"));   //23
-                    config.MutationConfig.UseCompletelyRandomDefaultGenome = reader.GetBoolean(reader.GetOrdinal("randomDefault"));  //24
-                    config.MutationConfig.DefaultGenome = reader.GetString(reader.GetOrdinal("defaultGenome"));   //25
+                    config.MatchConfig = ReadMatchConfig(reader, 12);//TODO check index
+                    config.MutationConfig = ReadMutationConfig(reader, 13);//TODO check index
                 }
                 catch (Exception e)
                 {
@@ -103,15 +71,7 @@ namespace Assets.Src.Database
                 }
                 finally
                 {
-                    //Debug.Log("Disconnecting");
-                    if (reader != null)
-                        reader.Close();
-                    reader = null;
-                    if (dbcmd != null)
-                        dbcmd.Dispose();
-                    dbcmd = null;
-                    if (sql_con != null)
-                        sql_con.Close();
+                    Disconnect(reader, null, dbcmd, sql_con);
                 }
             }
             return config;
@@ -127,16 +87,8 @@ namespace Assets.Src.Database
                 IDataReader reader = null;
                 try
                 {
-                    sql_con.Open(); //Open connection to the database.
-                    dbcmd = sql_con.CreateCommand();
-                    string sqlQuery = "SELECT *" +
-                        " FROM DroneShootingIndividual" +
-                        " WHERE runConfigId = " + runId + " AND generation = " + generationNumber +
-                        ";";
-                    //Debug.Log(sqlQuery);
-                    dbcmd.CommandText = sqlQuery;
-                    reader = dbcmd.ExecuteReader();
-                    
+                    reader = OpenReaderWithCommant(sql_con, CreateReadIndividualsQuery(INDIVIDUAL_TABLE, runId, generationNumber), out dbcmd);
+
                     while (reader.Read())
                     {
                         //Debug.Log("genome ordinal: " + reader.GetOrdinal("genome"));  //-1
@@ -162,15 +114,7 @@ namespace Assets.Src.Database
                 }
                 finally
                 {
-                    //Debug.Log("Disconnecting");
-                    if (reader != null)
-                        reader.Close();
-                    reader = null;
-                    if (dbcmd != null)
-                        dbcmd.Dispose();
-                    dbcmd = null;
-                    if (sql_con != null)
-                        sql_con.Close();
+                    Disconnect(reader, null, dbcmd, sql_con);
                 }
             }
 
@@ -220,15 +164,7 @@ namespace Assets.Src.Database
                 }
                 finally
                 {
-                    //Debug.Log("Disconnecting");
-                    if (transaction != null)
-                        transaction.Dispose();
-                    transaction = null;
-                    if (dbcmd != null)
-                        dbcmd.Dispose();
-                    dbcmd = null;
-                    if (sql_con != null)
-                        sql_con.Close();
+                    Disconnect(null, transaction, dbcmd, sql_con);
                 }
             }
         }
@@ -259,48 +195,11 @@ namespace Assets.Src.Database
                 }
                 finally
                 {
-                    //Debug.Log("Disconnecting");
-                    if (transaction != null)
-                        transaction.Dispose();
-                    transaction = null;
-                    if (dbcmd != null)
-                        dbcmd.Dispose();
-                    dbcmd = null;
-                    if (sql_con != null)
-                        sql_con.Close();
+                    Disconnect(null, transaction, dbcmd, sql_con);
                 }
             }
         }
-
-        //YAGNI: might be useful in future
-        //public void UpdateIndividual(IndividualTargetShooting individual, int runId, int generationNumber)
-        //{
-        //    using (var sql_con = new SqliteConnection(_connectionString))
-        //    {
-        //        IDbCommand dbcmd = null;
-        //        try
-        //        {
-        //            sql_con.Open(); //Open connection to the database.
-                                        
-        //            UpdateIndividual(individual, runId, generationNumber, sql_con, null);
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            Debug.LogWarning("Caught exception: " + e + ", message: " + e.Message);
-        //            throw e;
-        //        }
-        //        finally
-        //        {
-        //            //Debug.Log("Disconnecting");
-        //            if (dbcmd != null)
-        //                dbcmd.Dispose();
-        //            dbcmd = null;
-        //            if (sql_con != null)
-        //                sql_con.Close();
-        //        }
-        //    }
-        //}
-
+        
         private void UpdateIndividual(IndividualTargetShooting individual, int runId, int generationNumber, SqliteConnection sql_con, SqliteTransaction transaction)
         {
             SqliteCommand insertSQL = new SqliteCommand("UPDATE  DroneShootingIndividual " +
@@ -326,170 +225,7 @@ namespace Assets.Src.Database
 
         public void SetCurrentGenerationNumber(int databaseId, int generationNumber)
         {
-            using (var sql_con = new SqliteConnection(_connectionString))
-            {
-                sql_con.Open();
-                SqliteCommand command = null;
-
-                try
-                {
-                    //Debug.Log("Updating generation to " + config.GenerationNumber);
-                    command = new SqliteCommand("UPDATE DroneEvolutionConfig SET currentGeneration = ? WHERE id = ?;", sql_con);
-
-                    command.Parameters.Add(new SqliteParameter(DbType.Int32, (object)generationNumber));
-                    command.Parameters.Add(new SqliteParameter(DbType.Int32, (object)databaseId));
-
-                    command.ExecuteNonQuery();
-                }
-                finally
-                {
-                    if (command != null)
-                        command.Dispose();
-                    if (sql_con != null)
-                        sql_con.Close();
-
-                }
-            }
-        }
-
-        public void SaveConfig()
-        {
-            throw new NotImplementedException();
-            //using (var sql_con = new SqliteConnection(_connectionString))
-            //{
-            //    sql_con.Open();
-            //    try
-            //    {
-            //        SaveMatchConfig(config.MatchControl, sql_con);
-            //        SaveMutationConfig(config.MutationControl, sql_con);
-
-            //        SaveEvolutionControlerConfig(sql_con);
-            //    }
-            //    finally
-            //    {
-            //        if (sql_con != null)
-            //            sql_con.Close();
-            //    }
-            //}
-        }
-
-        private int? SaveMatchConfig(EvolutionMatchController matchConfig, SqliteConnection sql_con)
-        {
-            throw new NotImplementedException();
-            //if (matchConfig.Id.HasValue)
-            //{
-            //    Debug.LogWarning("Updating existing MatchConfig not implemented, Id: " + matchConfig.Id.Value);
-            //}
-            //else
-            //{
-            //    string sqlQuery = "SELECT id" +
-            //    " FROM MatchConfig;";
-
-            //    var dbcmd = sql_con.CreateCommand();
-            //    dbcmd.CommandText = sqlQuery;
-            //    var reader = dbcmd.ExecuteReader();
-
-            //    var ids = new List<int>();
-            //    while (reader.Read())
-            //    {
-            //        ids.Add(reader.GetInt32(0));
-            //    }
-            //    var maxId = ids.Max();
-            //    var newId = maxId + 1;
-
-            //    matchConfig.Id = newId;
-
-            //    SqliteCommand insertSQL = new SqliteCommand("INSERT INTO MatchConfig (id, matchTimeout, winnerPollPeriod) VALUES (?,?,?)", sql_con);
-
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)matchConfig.Id));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)matchConfig.MatchTimeout));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)matchConfig.WinnerPollPeriod));
-
-            //    insertSQL.ExecuteNonQuery();
-            //}
-            //return matchConfig.Id;
-        }
-
-        private int? SaveMutationConfig(EvolutionMutationController mutationConfig, SqliteConnection sql_con)
-        {
-            throw new NotImplementedException();
-            //if (mutationConfig.Id.HasValue)
-            //{
-            //    Debug.LogWarning("Updating existing MutationConfig not implemented, Id: " + mutationConfig.Id.Value);
-            //}
-            //else
-            //{
-            //    sql_con = new SqliteConnection(_connectionString);
-            //    sql_con.Open(); //Open connection to the database.
-
-            //    string sqlQuery = "SELECT id" +
-            //    " FROM MutationConfig;";
-
-            //    var dbcmd = sql_con.CreateCommand();
-            //    dbcmd.CommandText = sqlQuery;
-            //    var reader = dbcmd.ExecuteReader();
-
-            //    var ids = new List<int>();
-            //    while (reader.Read())
-            //    {
-            //        ids.Add(reader.GetInt32(0));
-            //    }
-            //    var maxId = ids.Max();
-            //    var newId = maxId + 1;
-            //    mutationConfig.Id = newId;
-
-            //    SqliteCommand insertSQL = new SqliteCommand("INSERT INTO MutationConfig (" +
-            //        "id, mutations, allowedCharacters, maxMutationLength" +
-            //        ", genomeLength, generationSize, randomDefault, defaultGenome) VALUES (?,?,?,?,?,?,?,?)", sql_con);
-
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)mutationConfig.Id.Value));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)mutationConfig.Mutations));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.String, (object)mutationConfig.AllowedCharacters));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)mutationConfig.MaxMutationLength));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)mutationConfig.GenomeLength));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)mutationConfig.GenerationSize));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Boolean, (object)mutationConfig.UseCompletelyRandomDefaultGenome));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.String, (object)mutationConfig.DefaultGenome));
-
-            //    insertSQL.ExecuteNonQuery();
-
-            //}
-            //return mutationConfig.Id;
-        }
-
-        private void SaveEvolutionControlerConfig(SqliteConnection sql_con)
-        {
-            throw new NotImplementedException();
-            //if (config.ReadFromDatabase)
-            //{
-            //    //If it's set to be read from the database, it must already exist in the database, so update the existing one.
-            //    Debug.LogWarning("Updating existing DroneEvolutionConfig not implemented, Id: " + config.DatabaseId);
-            //}
-            //else
-            //{
-            //    SqliteCommand insertSQL = new SqliteCommand("INSERT INTO DroneEvolutionConfig (name , currentGeneration , minMatchesPerIndividual" +
-            //        ", winnersCount , minDrones , droneEscalation , maxDrones , killScoreMultiplier, flatKillBonus, completionBonus " +
-            //        ", deathPenalty, droneList, matchConfigId, mutationConfigId) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", sql_con);
-
-            //    //insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)config.Id.Value));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.String, (object)config.RunName));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)config.GenerationNumber));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)config.MinMatchesPerIndividual));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)config.WinnersFromEachGeneration));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)config.MinDronesToSpawn));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)config.ExtraDroneEveryXGenerations));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)config.MaxDronesToSpawn));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Decimal, (object)config.KillScoreMultiplier));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Decimal, (object)config.FlatKillBonus));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Decimal, (object)config.CompletionBonus));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Decimal, (object)config.DeathPenalty));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.String, (object)config.DronesString));
-
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)config.MatchControl.Id));
-            //    insertSQL.Parameters.Add(new SqliteParameter(DbType.Int32, (object)config.MutationControl.Id));
-
-            //    insertSQL.ExecuteNonQuery();
-            //}
+            SetCurrentGenerationNumber(CONFIG_TABLE, databaseId, generationNumber);
         }
     }
 }
---
+++ Assets/Src/Database/GeneralDatabaseHandler.cs
@@ -0,0 +1,145 @@
+﻿using Assets.src.Evolution;
+using Assets.Src.Evolution;
+using Mono.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Src.Database
+{
+    public abstract class GeneralDatabaseHandler
+    {
+        private const string DEFAULT_COMMAND_PATH = "/Database/CreateBlankDatabase.sql";
+        private const string DEFAULT_DB_PATH = "/tmp/SpaceCombatSimulationDB.s3db";
+        protected string _connectionString
+        {
+            get
+            {
+                var connection = "URI=file:" + Application.dataPath + _databasePath;
+                //Debug.Log("connection string: " + connection);
+                return connection;
+            }
+        }
+        protected string _databasePath; //Path to database.
+
+        public GeneralDatabaseHandler(string databasePath = DEFAULT_DB_PATH, string dbCreationCommandPath = DEFAULT_COMMAND_PATH)
+        {
+            _databasePath = databasePath;
+
+            if (!string.IsNullOrEmpty(dbCreationCommandPath))
+            {
+                var initialiser = new DatabaseInitialiser
+                {
+                    DatabasePath = _databasePath
+                };
+                initialiser.EnsureDatabaseExists(dbCreationCommandPath);
+            }
+        }
+
+        protected string CreateReadConfigQuery(string table, int id)
+        {
+            string sqlQuery = "SELECT *" +
+                        " FROM " + table +
+                        " LEFT JOIN MatchConfig on MatchConfig.id = " + table + ".matchConfigId" +
+                        " LEFT JOIN MutationConfig on MutationConfig.id = " + table + ".mutationConfigId" +
+                        " WHERE " + table + ".id = " + id + ";";
+            return sqlQuery;
+        }
+
+        protected string CreateReadIndividualsQuery(string table, int runId, int generationNumber)
+        {
+            string sqlQuery = "SELECT *" +
+                        " FROM " + table +
+                        " WHERE runConfigId = " + runId + " AND generation = " + generationNumber +
+                        ";";
+            return sqlQuery;
+        }
+
+        protected IDataReader OpenReaderWithCommant(SqliteConnection connection, string command, out IDbCommand dbcmd)
+        {
+            connection.Open(); //Open connection to the database.
+            dbcmd = connection.CreateCommand();
+            dbcmd.CommandText = command;
+            var reader = dbcmd.ExecuteReader();
+
+            return reader;
+        }
+
+        protected MatchConfig ReadMatchConfig(IDataReader reader, int idIndex)
+        {
+            //Debug.Log("matchConfigId ordinal: " + reader.GetOrdinal("MatchConfig.Id"));  //-1
+
+            var config = new MatchConfig()
+            {
+                Id = reader.GetInt32(idIndex),  //TODO check this
+                MatchTimeout = reader.GetFloat(reader.GetOrdinal("matchTimeout")), //16
+                WinnerPollPeriod = reader.GetFloat(reader.GetOrdinal("winnerPollPeriod")) //17
+            };
+            return config;
+        }
+
+        protected MutationConfig ReadMutationConfig(IDataReader reader, int idIndex)
+        {
+            //Debug.Log("matchConfigId ordinal: " + reader.GetOrdinal("MatchConfig.Id"));  //-1
+
+            var config = new MutationConfig()
+            {
+                Id = reader.GetInt32(idIndex),
+                Mutations = reader.GetInt32(reader.GetOrdinal("mutations")),
+                AllowedCharacters = reader.GetString(reader.GetOrdinal("allowedCharacters")),
+                MaxMutationLength = reader.GetInt32(reader.GetOrdinal("maxMutationLength")),
+                GenomeLength = reader.GetInt32(reader.GetOrdinal("genomeLength")),
+                GenerationSize = reader.GetInt32(reader.GetOrdinal("generationSize")),
+                UseCompletelyRandomDefaultGenome = reader.GetBoolean(reader.GetOrdinal("randomDefault")),
+                DefaultGenome = reader.GetString(reader.GetOrdinal("defaultGenome"))
+            };
+            return config;
+        }
+
+        protected void Disconnect(IDataReader reader, SqliteTransaction transaction , IDbCommand dbcmd, SqliteConnection sql_con)
+        {
+            //Debug.Log("Disconnecting");
+            if (reader != null)
+                reader.Close();
+            reader = null;
+
+            if (transaction != null)
+                transaction.Dispose();
+            transaction = null;
+
+            if (dbcmd != null)
+                dbcmd.Dispose();
+            dbcmd = null;
+
+            if (sql_con != null)
+                sql_con.Close();
+        }
+
+        protected void SetCurrentGenerationNumber(string table, int databaseId, int generationNumber)
+        {
+            using (var sql_con = new SqliteConnection(_connectionString))
+            {
+                sql_con.Open();
+                SqliteCommand command = null;
+
+                try
+                {
+                    //Debug.Log("Updating generation to " + config.GenerationNumber);
+                    command = new SqliteCommand("UPDATE " + table + " SET currentGeneration = ? WHERE id = ?;", sql_con);
+                    
+                    command.Parameters.Add(new SqliteParameter(DbType.Int32, (object)generationNumber));
+                    command.Parameters.Add(new SqliteParameter(DbType.Int32, (object)databaseId));
+
+                    command.ExecuteNonQuery();
+                }
+                finally
+                {
+                    Disconnect(null, null, command, sql_con);
+                }
+            }
+        }
+    }
+}
