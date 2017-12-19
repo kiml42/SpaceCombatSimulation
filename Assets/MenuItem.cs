eb5981f9a783454546962f809dc70f82298042ce
Richard Clacksman <kiml42a@gmail.com>
2017-12-19 14:56:13 +0000

    Started work on main menu

---
+++ Assets/MenuItem.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Src.ObjectManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuItem : MonoBehaviour {
+    public Color NormalColour = Color.white;
+    public Color HighlightColour = Color.yellow;
+
+
+	// Use this for initialization
+	void Start () {
+        transform.SetColor(NormalColour);
+	}
+
+    public void OnMouseEnter()
+    {
+        transform.SetColor(HighlightColour);
+    }
+
+    public void OnMouseExit()
+    {
+        transform.SetColor(NormalColour);
+    }
+
+    public void OnMouseUp()
+    {
+        Debug.Log("MouseUp " + name);
+    }
+}
