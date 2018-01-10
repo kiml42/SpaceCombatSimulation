6281f1b9e87f1ccc6d0b2f5cbfd31efcb84a970c
kiml42 <kiml42a@gmail.com>
2018-01-10 12:58:05 +0000

    Created ModuleTypeKnower and asigned it to all relevant prefabs.

---
+++ Assets/Src/ModuleSystem/ModuleType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Src.ModuleSystem
+{
+    public enum ModuleType
+    {
+        Hub, Turret, Engine
+    }
+}
---
+++ Assets/Src/ModuleSystem/ModuleTypeKnower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Src.ModuleSystem
+{
+    public class ModuleTypeKnower : MonoBehaviour
+    {
+        [Tooltip("the list of types that this module can act as.")]
+        public List<ModuleType> Types;
+
+        [Tooltip("the cost for this module when evolving ships.")]
+        public float Cost = 100;
+    }
+}
