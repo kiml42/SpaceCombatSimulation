c6053ca1af2d2c5e1e953393a0dde738cd4880ed
kiml42 <kiml42a@gmail.com>
2018-02-12 16:39:48 +0000

    Added tests with randomised scores.

--- SpaceCombatSimulation/Assets/Editor/Evolution/GenerationTests.cs
+++ SpaceCombatSimulation/Assets/Editor/Evolution/GenerationTests.cs
@@ -68,11 +68,10 @@ public class GenerationTests
     [Test]
     public void PickWinners_PicksWithAReasonableDistribution_20_1()
     {
-        var runs = 5000;
         var count = 20;
         var winnersCount = 1;
 
-        TestPickWinnersDistribution(count, winnersCount, runs);
+        TestPickWinnersDistribution(count, winnersCount);
         //no assertion here because it won't nessersarily come up with a negative.
     }
 
@@ -98,6 +97,71 @@ public class GenerationTests
 
         Assert.Less(Min(all), 0);
         Assert.Contains(3, all);
+    }
+
+    [Test]
+    public void PickWinners_PicksWithAReasonableDistribution_50_23_moreRandom()
+    {
+        var count = 50;
+        var winnersCount = 23;
+
+        var all = TestPickWinnersDistribution_MoreRandom(count, winnersCount);
+
+        Assert.Less(all.Min(), 0);
+    }
+
+    [Test]
+    public void PickWinners_PicksWithAReasonableDistribution_20_7_moreRandom()
+    {
+        var count = 20;
+        var winnersCount = 7;
+
+        var all = TestPickWinnersDistribution_MoreRandom(count, winnersCount);
+
+        Assert.Less(all.Min(), 0);
+    }
+
+    [Test]
+    public void PickWinners_PicksWithAReasonableDistribution_50_25_moreRandom()
+    {
+        var count = 50;
+        var winnersCount = 25;
+
+        var all = TestPickWinnersDistribution_MoreRandom(count, winnersCount);
+
+        Assert.Less(all.Min(), 0);
+    }
+
+    [Test]
+    public void PickWinners_PicksWithAReasonableDistribution_20_1_moreRandom()
+    {
+        var count = 20;
+        var winnersCount = 1;
+
+        TestPickWinnersDistribution_MoreRandom(count, winnersCount);
+        //no assertion here because it won't nessersarily come up with a negative.
+    }
+
+    [Test]
+    public void PickWinners_PicksWithAReasonableDistribution_20_10_moreRandom()
+    {
+        var count = 20;
+        var winnersCount = 10;
+
+        var all = TestPickWinnersDistribution_MoreRandom(count, winnersCount);
+
+        Assert.Less(all.Min(), 0);
+    }
+
+    [Test]
+    public void PickWinners_PicksWithAReasonableDistribution_20_5_moreRandom()
+    {
+        var count = 20;
+        var winnersCount = 5;
+
+        var all = TestPickWinnersDistribution_MoreRandom(count, winnersCount);
+
+        Assert.Less(all.Min(), 0);
     }
 
     private List<int> TestPickWinnersDistribution(int generationSize, int winnersCount, int runs = 1000)
@@ -139,6 +203,56 @@ public class GenerationTests
         foreach (var group in groups.OrderByDescending(g => g.Key))
         {
             Debug.Log((group.Key.ToString()).PadRight(5) +
+                (" (" + group.Count().ToString() + ")").PadRight(8) +
+                "".PadRight(group.Count() * 80 / runs, '|'));
+        }
+
+        return all;
+    }
+
+    private List<float> TestPickWinnersDistribution_MoreRandom(int generationSize, int winnersCount, int runs = 1000)
+    {
+        Debug.Log(runs + "runs, " + generationSize + " individuals, " + winnersCount + " winners");
+        var all = new List<float>();
+        var maxes = new List<float>();
+        var mins = new List<float>();
+
+        var gen = new GenerationTargetShooting();
+
+        var random = new System.Random();
+
+        for (int i = 0; i < generationSize; i++)
+        {
+            var score = (float)(0.5 - random.NextDouble()) * 1000;
+
+            gen.AddGenome(score.ToString());
+
+            gen.RecordMatch(new GenomeWrapper(score.ToString()), score, true, true, 1);
+        }
+
+        for (int j = 0; j < runs; j++)
+        {
+
+            var winners = gen.PickWinners(winnersCount);
+
+            var ints = winners.Select(g => float.Parse(g)).ToList();
+
+            all.AddRange(ints);
+            maxes.Add(ints.Max());
+            mins.Add(ints.Min());
+        }
+        Debug.Log("max max: " + maxes.Max() + ", min max: " + maxes.Min());
+        Debug.Log("max min: " + mins.Max() + ", min min: " + mins.Min());
+        Debug.Log("max all: " + all.Max() + ", min all: " + all.Min());
+
+        //Debug.Log("all:  " + string.Join(",",  all.OrderBy(x => x).Select(x => x.ToString()).Take(30).ToArray()));
+        //Debug.Log("mins: " + string.Join(",", mins.OrderBy(x => x).Select(x => x.ToString()).Take(30).ToArray()));
+
+        var groups = all.GroupBy(i => i);
+
+        foreach (var group in groups.OrderByDescending(g => g.Key))
+        {
+            Debug.Log((group.Key.ToString()).PadRight(10) +
                 (" (" + group.Count().ToString() + ")").PadRight(8) +
                 "".PadRight(group.Count() * 80 / runs, '|'));
         }
