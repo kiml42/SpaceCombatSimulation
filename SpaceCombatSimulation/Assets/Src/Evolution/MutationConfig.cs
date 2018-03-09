d6604a0fec1f5c81f09332ea31e40aba841700ef
Richard Clacksman <kiml42a@gmail.com>
2018-03-09 21:11:30 +0000

    Improved defaults

--- SpaceCombatSimulation/Assets/Src/Evolution/MutationConfig.cs
+++ SpaceCombatSimulation/Assets/Src/Evolution/MutationConfig.cs
@@ -2,13 +2,13 @@
 {
     public class MutationConfig
     {
-        public int Mutations = 20;
+        public int Mutations = 40;
 
         public int MaxMutationLength = 10;
 
         public int GenomeLength = 1000;
 
-        public int GenerationSize = 20;
+        public int GenerationSize = 25;
 
         public bool UseCompletelyRandomDefaultGenome = false;
 
