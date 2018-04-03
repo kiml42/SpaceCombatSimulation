cd178c731d08eded5684c018f2a346f94e171c14
kiml42 <kiml42a@gmail.com>
2018-04-03 13:07:43 +0100

    Extracted graphing to separate behaviour

---
+++ SpaceCombatSimulation/Assets/Src/Graph/BaseGraphDrawer.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Src.Evolution;
+using UnityEngine;
+
+namespace Assets.Src.Graph
+{
+    public abstract class BaseGraphDrawer: MonoBehaviour
+    {
+        public Texture BorderTexture;
+        public Texture PointTexture;
+
+        public Rect GraphRect = new Rect(50, 50, 450, 150);
+
+        public BaseEvolutionController EvolutionControler;
+
+        public KeyCode DrawGraphKey = KeyCode.G;
+
+        protected abstract bool _hasCalculatedGraph { get; }
+
+        public void OnGUI()
+        {
+            if (Input.GetKeyUp(DrawGraphKey) && !_hasCalculatedGraph)
+            {
+                PrepareGraph();
+            }
+            if (_hasCalculatedGraph)
+            {
+                DrawGraph();
+            }
+        }
+
+        internal abstract void DrawGraph();
+        internal abstract void PrepareGraph();
+    }
+}
---
+++ SpaceCombatSimulation/Assets/Src/Graph/ScoreGraphDrawer.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Assets.Src.Graph
+{
+    public class ScoreGraphDrawer : BaseGraphDrawer
+    {
+        private LineGraph _graph;
+
+        protected override bool _hasCalculatedGraph
+        {
+            get
+            {
+                return _graph != null;
+            }
+        }
+
+        internal override void DrawGraph()
+        {
+            if(_graph != null)
+            {
+                _graph.DrawGraph(GraphRect, BorderTexture, PointTexture);
+            }
+        }
+
+        internal override void PrepareGraph()
+        {
+            var generations = Enumerable.Range(0, EvolutionControler.GenerationNumber)
+                .ToDictionary(i => i, i => EvolutionControler.DbHandler.ReadBaseGeneration(EvolutionControler.DatabaseId, i));
+
+            var minScore = new GraphLine { Colour = Color.red };
+            var avgScore = new GraphLine { Colour = Color.magenta };
+            var maxScore = new GraphLine { Colour = Color.green };
+
+            foreach (var generation in generations)
+            {
+                minScore.Add(generation.Key, generation.Value.MinScore);
+                avgScore.Add(generation.Key, generation.Value.AvgScore);
+                maxScore.Add(generation.Key, generation.Value.MaxScore);
+            }
+
+            _graph = new LineGraph(/*minScore,*/ avgScore, maxScore);
+        }
+    }
+}
