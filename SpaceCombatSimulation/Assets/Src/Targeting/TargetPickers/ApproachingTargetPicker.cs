3feb2dce9119d86cf30dffd88700b32f0e6302a1
Richard Clacksman <richard@digital-morphosis.com>
2019-05-07 15:41:29 +0100

    Fixed turret prefabs

--- SpaceCombatSimulation/Assets/Src/Targeting/TargetPickers/ApproachingTargetPicker.cs
+++ SpaceCombatSimulation/Assets/Src/Targeting/TargetPickers/ApproachingTargetPicker.cs
@@ -1,5 +1,4 @@
-﻿using Assets.Src.Evolution;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -17,7 +16,7 @@ namespace Assets.Src.Targeting.TargetPickers
 
         private PotentialTarget AddScoreForDifference(PotentialTarget target)
         {
-            Vector3 targetVelocity = target.Rigidbody == null ? Vector3.zero : target.Rigidbody.velocity;
+            var targetVelocity = target.Rigidbody == null ? Vector3.zero : target.Rigidbody.velocity;
 
             var relativeVelocity = SourceObject.velocity - targetVelocity;
 
