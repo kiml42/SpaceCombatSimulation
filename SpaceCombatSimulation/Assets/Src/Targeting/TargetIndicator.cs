012519b589d141d5ac98e019e9992c1c1808e0e7
Richard Clacksman <kiml42a@gmail.com>
2019-11-22 19:40:40 +0000

    Improved null checking

--- SpaceCombatSimulation/Assets/Src/Targeting/TargetIndicator.cs
+++ SpaceCombatSimulation/Assets/Src/Targeting/TargetIndicator.cs
@@ -17,7 +17,7 @@ public class TargetIndicator : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-        if(SourceObject != null && TargetKnower != null && TargetKnower.CurrentTarget != null)
+        if(SourceObject != null && TargetKnower != null && TargetKnower.CurrentTarget?.Transform != null )
         {
             TargetingLine.SetPosition(0, SourceObject.transform.position);
             TargetingLine.SetPosition(1, TargetKnower.CurrentTarget.Transform.position);
