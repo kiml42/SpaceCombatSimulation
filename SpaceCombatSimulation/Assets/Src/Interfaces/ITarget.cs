6ff2add0f70a5b2c66a43ae23812d366fbd6635e
Richard Clacksman <kiml42a@gmail.com>
2019-12-06 21:12:43 +0000

    Added IIarget

---
+++ SpaceCombatSimulation/Assets/Src/Interfaces/ITarget.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Src.Targeting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Src.Interfaces
+{
+    public interface ITarget
+    {
+        Transform Transform { get; }
+        Rigidbody Rigidbody { get; }
+        ShipType Type { get; }
+    }
+}
