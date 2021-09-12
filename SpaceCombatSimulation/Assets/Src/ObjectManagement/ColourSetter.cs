3f34e502b56130fb3f81616b61937d3bc5d701be
Richard Clacksman <rc@wordwall.co.uk>
2021-09-12 13:54:04 +0100

    Fixed setting colours of spawned objects.

--- SpaceCombatSimulation/Assets/Src/ObjectManagement/ColourSetter.cs
+++ SpaceCombatSimulation/Assets/Src/ObjectManagement/ColourSetter.cs
@@ -1,15 +1,29 @@
 ﻿using Assets.Src.ObjectManagement;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ColourSetter : MonoBehaviour
 {
+    [Tooltip("if true, this will use its parent's colour setter's colour instead.")]
+    public bool GetColourFromParent = true;
+
     public Color Colour;
 
     // Use this for initialization
     void Start()
     {
+        if (GetColourFromParent && transform.parent != null)
+        {
+            var parentColourSetter = transform.parent.GetComponentInParent<ColourSetter>();
+            if(parentColourSetter != null)
+            {
+                Colour = parentColourSetter.Colour;
+            }
+        }
         transform.SetColor(Colour);
     }
+
+    //private void Update()
+    //{
+    //    transform.SetColor(Colour);
+    //}
 }
