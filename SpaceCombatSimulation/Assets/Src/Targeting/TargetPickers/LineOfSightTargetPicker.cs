25f298248d22f8e836056d2a111da39d293b91a7
Richard Clacksman <rc@wordwall.co.uk>
2021-10-10 13:30:18 +0100

    Improved null handling.

--- SpaceCombatSimulation/Assets/Src/Targeting/TargetPickers/LineOfSightTargetPicker.cs
+++ SpaceCombatSimulation/Assets/Src/Targeting/TargetPickers/LineOfSightTargetPicker.cs
@@ -17,12 +17,11 @@ namespace Assets.Src.Targeting.TargetPickers
 
         public override IEnumerable<PotentialTarget> FilterTargets(IEnumerable<PotentialTarget> potentialTargets)
         {
-            potentialTargets =  potentialTargets.Select(t => {
+            potentialTargets =  potentialTargets.Where(t => t?.Target?.Transform != null).Select(t => {
                 var direction = t.Target.Transform.position - SourceObject.position;
 
-                RaycastHit hit;
                 var ray = new Ray(SourceObject.position + (direction * MinDetectionDistance), direction);
-                if (Physics.Raycast(ray, out hit, direction.magnitude, -1, QueryTriggerInteraction.Ignore))
+                if (Physics.Raycast(ray, out var hit, direction.magnitude, -1, QueryTriggerInteraction.Ignore))
                 {
                     //is a hit - should always be a hit, because it's aimed at an object
                     if (hit.transform == t.Target.Transform)
