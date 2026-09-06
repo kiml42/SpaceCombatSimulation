00fdf2d080ad49268bbbf28350c55f57f58006a0
Richard Clacksman <kiml42a@gmail.com>
2026-09-06 13:41:51 +0100

    Fixed 2 tests

--- SCS2D/tests/golden.test.ts
+++ SCS2D/tests/golden.test.ts
@@ -27,7 +27,7 @@ const GOLDEN: Record<ScenarioName, string> = {
   tumble: '52118178',
   gunnery: 'c3e9551c',
   duel: '5f051125',
-  swarm: '27e8368f',
+  swarm: '05b159ef',
 };
 
 describe('golden scenarios', () => {
