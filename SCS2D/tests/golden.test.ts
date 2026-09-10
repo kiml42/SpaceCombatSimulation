be7705cb5ec83cf474d782d351ef705c4a3b6461
Richard Clacksman <kiml42a@gmail.com>
2026-09-10 15:27:45 +0100

    Update golden.test.ts

--- SCS2D/tests/golden.test.ts
+++ SCS2D/tests/golden.test.ts
@@ -29,7 +29,7 @@ const GOLDEN: Record<ScenarioName, string> = {
   duel: '9b5b83ab',
   swarm: '366a71dd',
   superSwarm: '19ec9034',
-  fractal: 'b080e616',
+  fractal: '6f440886',
 };
 
 describe('golden scenarios', () => {
