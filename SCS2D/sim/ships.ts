68e36a66fd43d380467d0185c5fa3354b221140c
Richard Clacksman <kiml42a@gmail.com>
2026-09-17 22:49:25 +0100

    more todos

--- SCS2D/sim/ships.ts
+++ SCS2D/sim/ships.ts
@@ -368,8 +368,9 @@ export class Ships {
   /** Hold station on another ship within a range band. */
   pushOrder(i: number, target: number, minRange: number, maxRange: number, approachSpeed: number): void {
     // TODO set threshold for how damaged it needs to be (e.g. min remaining turrets)
-    // TODO have Fit only include non-disabled ships
+    // TODO have Fit only include non-disabled ships (possibly only focus on a ship and its target.)
     // TODO stop drawing firing arcs for disabled turrets
+    // TODO Update orders once per step so that there can be an easy way to access the current order without changing anything.
     const order = {
       target: target,
       minRange: minRange,
